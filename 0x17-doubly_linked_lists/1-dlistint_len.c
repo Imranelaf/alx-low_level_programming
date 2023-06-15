ca74ddae1c886b6454cb465c48aa1e080d1586ce
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-06-15 12:01:14 -0700

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Returns the number of nodes in a doubly linked list
- * @h: Pointer to the head of the list
- *
- * Return: Number of nodes in the list
- */
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ **/
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *n = h;	
-    size_t count = 0;
+	const dlistint_t *node = h;
+	size_t cont = 0;
 
-    while (n != NULL)
-    {
-        count++;
-        n = n->next;
-    }
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
 
-    return count;
+	return (cont);
 }
