085707b4a74c550491952ef569e043a5d2bbf1e6
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-06-15 16:10:25 -0700

    crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,45 +1,45 @@
-#include <stdio.h>
+include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char *argv[])
 {
-	unsigned int index, sum;
-	size_t length, additional;
-	char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char key[7] = "      ";
+	unsigned int i, b;
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char p[7] = "      ";
 
 	if (argc != 2)
 	{
 		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-
-	length = strlen(argv[1]);
-	key[0] = charset[(length ^ 59) & 63];
-
-	for (index = 0, additional = 0; index < length; index++)
-		additional += argv[1][index];
-	key[1] = charset[(additional ^ 79) & 63];
-
-	for (index = 0, sum = 1; index < length; index++)
-		sum *= argv[1][index];
-	key[2] = charset[(sum ^ 85) & 63];
-
-	for (sum = argv[1][0], index = 0; index < length; index++)
-		if ((char)sum <= argv[1][index])
-			sum = argv[1][index];
-	srand(sum ^ 14);
-	key[3] = charset[rand() & 63];
-
-	for (sum = 0, index = 0; index < length; index++)
-		sum += argv[1][index] * argv[1][index];
-	key[4] = charset[(sum ^ 239) & 63];
-
-	for (sum = 0, index = 0; (char)index < argv[1][0]; index++)
-		sum = rand();
-	key[5] = charset[(sum ^ 229) & 63];
-
-	printf("%s\n", key);
+	len = strlen(argv[1]);
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0, add = 0; i < len; i++)
+		add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+		b *= argv[1][i];
+	p[2] = l[(b ^ 85) & 63];
+	for (b = argv[1][0], i = 0; i < len; i++)
+		if ((char)b <= argv[1][i])
+			b = argv[1][i];
+	srand(b ^ 14);
+	p[3] = l[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+		b += argv[1][i] * argv[1][i];
+	p[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+		b = rand();
+	p[5] = l[(b ^ 229) & 63];
+	printf("%s\n", p);
 	return (0);
 }
