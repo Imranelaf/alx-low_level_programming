45dec0461db88a98d5d3e5c7cfac34dce3455970
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-10-11 14:08:00 -0700

    Jump search in a singly linked list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,7 +12,6 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for ALX project
  */
 typedef struct listint_s
 {
@@ -30,7 +29,6 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * for Holberton project
  */
 typedef struct skiplist_s
 {
@@ -41,11 +39,15 @@ typedef struct skiplist_s
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
+int recursive_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
 int exponential_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recur(int *array, size_t left, size_t right, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */
